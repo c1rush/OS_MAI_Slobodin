b8f8c5534b4641a915c8fdcc4fbc6e15ef0f2cee
c1rush <nikitaslobodin05@gmail.com>
2024-12-27 23:34:27 +0000

    Added docs

--- lab4/program2.cpp
+++ lab4/program2.cpp
@@ -82,7 +82,7 @@ int main() {
 }
 
 
-// export PATH_TO_LIB1=$(pwd)/lab4/lib1.dylib
-// export PATH_TO_LIB2=$(pwd)/lab4/lib2.dylib
+// export PATH_TO_LIB1=$(pwd)/lab4/lib1.so
+// export PATH_TO_LIB2=$(pwd)/lab4/lib2.so
 // echo $PATH_TO_LIB1
 // echo $PATH_TO_LIB2
